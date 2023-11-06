79f481b205795990c565bb46aed2259d035fe561
DRISSI El Houcine <116565465+ELhoucineDRISSI@users.noreply.github.com>
2023-11-06 11:13:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import streamlit as st
-import pdfplumber
+from PyPDF2 import PdfFileReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -9,13 +9,6 @@ from langchain.llms import OpenAI
 
 # Load environment variables from .env file
 load_dotenv()
-
-def extract_text_from_pdf(pdf_file):
-    with pdfplumber.open(pdf_file) as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
 
 def main():
     st.set_page_config(page_title="Chat with your PDF file")
@@ -26,14 +19,17 @@ def main():
 
     # Extract the text from the PDF file
     if pdf is not None:
-        text = extract_text_from_pdf(pdf)
+        pdf_reader = PdfFileReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
 
         # Split the text into chunks
         char_text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
         text_chunks = char_text_splitter.split_text(text)
 
         # Create embeddings
-        embeddings = OpenAIEmbeddings()
+        embeddings = OpenAIEmbeddings()  # Check the OpenAIEmbeddings constructor arguments
         docsearch = FAISS.from_texts(text_chunks, embeddings)
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
